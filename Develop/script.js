4fc5230adf6b3a7a86c5bcce86c3c76152691ac4
Cade Ellsworth <caedmd2100@gmail.com>
2021-05-14 21:43:30 -0700

    created new remote origin

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,7 @@
 var currentDayEl = document.querySelector("#currentDay");
-var saveTaskBtn = document.querySelector(".saveBtn")
+var allTasksData = [
+
+]
 var currentHour = moment().format("H");
 var hours = ["9", "10", "11", "12", "1", "2", "3", "4", "5"];
 
@@ -24,23 +26,52 @@ function checkCurrentHour() {
         else {
             $(hourId).addClass("future");
         }
-        if (localStorage.getItem(hours[i])) {
-            var taskListEl = document.getElementById(hours[i]);
-            taskListEl.innerHTML = localStorage.getItem(hours[i]);
-        }
+        // if (localStorage.getItem("tasks")) {
+        //     var taskListEl = document.getElementById(hours[i]);
+        //     taskListEl.innerHTML = localStorage.getItem(hours[i]);
+        // }
 
+        // change to localStorage getItem task, turn task back using parse
+        // loop through, each object has id, set textarea value to it
     }
 }
 
-function saveTask() {
-    localStorage.setItem("task", hours[i].textContent);
+var savedTasks = localStorage.getItem("task");
+var parsedSavedTasks = JSON.parse(savedTasks);
+if (parsedSavedTasks !== null) {
+
+    for (var i = 0; i < parsedSavedTasks.length; i++) {
+        var targetTextArea = document.getElementById(parsedSavedTasks[i].time);
+        targetTextArea.value = parsedSavedTasks[i].data;
+    }
+
 }
 
-saveTaskBtn.addEventListener("click", saveTask);
+// save in an array of objects DONE
+// steps
+// give all buttons an id DONE
+// keep all data objects in an array DONE
+// 
+
+function saveTask(event) {
+    console.log(event.target.id);
+    var textAreaContent = event.target.id;
+    textAreaContent = document.getElementById(textAreaContent);
+    console.log(textAreaContent.previousElementSibling.value);
+    var savedContentObj = {
+        time: textAreaContent.previousElementSibling.id,
+        data: textAreaContent.previousElementSibling.value
+    }
+    allTasksData.push(savedContentObj);
+    localStorage.setItem("task", JSON.stringify(allTasksData));
+}
+
+var allSaveBtns = document.querySelectorAll(".saveBtn");
+
+for (var i = 0; i < allSaveBtns.length; i++) {
+    allSaveBtns[i].addEventListener("click", saveTask);
+    console.log("working");
+}
 
 checkCurrentHour();
-
-
-
-
 
